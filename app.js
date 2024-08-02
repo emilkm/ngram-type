955d37f61a42be5b40199a9fd618a902eb53fc1c
emilkm <emilkm@gmail.com>
2024-08-02 22:33:09 +1200

    highlight current word by ahnwarez

--- app.js
+++ app.js
@@ -80,6 +80,7 @@ var ngramTypeConfig = {
             isInputCorrect: true,
             rawWPM: 0,
             accuracy: 0,
+            currentWordIndex: 0,
         }
     },
     computed: {
@@ -328,6 +329,7 @@ var ngramTypeConfig = {
                 }
                 this.isInputCorrect = true;
                 this.hitsCorrect += 1;
+                this.currentWordIndex = 0;
             }
             else if (this.expectedPhrase !== typedPhrase.trimEnd()) {
                 if (this.data.soundIncorrectLetterEnabled) {
@@ -338,6 +340,20 @@ var ngramTypeConfig = {
                 this.isInputCorrect = false;
                 this.hitsWrong += 1;
             }
+
+            let expectedWords = this.expectedPhrase.split(/(\s+)/);
+            let typedWords = typedPhrase.split(/(\s+)/);
+
+            // determine the current word index that a user is typing
+            let currentWordIndex = 0;
+            for (let i = 0; i < typedWords.length; i++) {
+              if (typedWords[i] === expectedWords[i]) {
+                currentWordIndex = i + 1;
+              } else {
+                break;
+              }
+            }
+            this.currentWordIndex = currentWordIndex;
 
             if (typedPhrase.trimEnd() === this.expectedPhrase) {
                 var currentTime = new Date().getTime() / 1000;
